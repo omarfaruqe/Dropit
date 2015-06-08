fa38517b138e3bb6a62bddafe8f94c9d627520f2
Omar Faruqe <faruqe@Omars-MacBook-Pro.local>
2015-06-08 15:46:38 -0400

    first commit

---
+++ BezierPathsView.swift
@@ -0,0 +1,21 @@
+//
+//  BezierPathsView.swift
+//  Dropit
+//
+//  Created by Omar Faruqe on 2015-06-08.
+//  Copyright (c) 2015 Omar Faruqe. All rights reserved.
+//
+
+import UIKit
+
+class BezierPathsView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DropitBehavior.swift
@@ -0,0 +1,13 @@
+//
+//  DropitBehavior.swift
+//  Dropit
+//
+//  Created by Omar Faruqe on 2015-06-08.
+//  Copyright (c) 2015 Omar Faruqe. All rights reserved.
+//
+
+import UIKit
+
+class DropitBehavior: UIDynamicBehavior {
+   
+}
